72492d64ab5e85927bd63075baf183566d1eb0af
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2020-04-07 16:52:58 +0300

    fix: fix most of the bugs

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
         "es6": true
     },
     "extends": [
-        "eslint:recommended",
         "airbnb-base"
     ],
     "globals": {
--- script.js
+++ script.js
@@ -31,7 +31,7 @@ const buttonTextRussian = [{ code: 'Backquote', lowerCase: 'ё', upperCase: 'Ё'
   { code: 'ControlLeft', lowerCase: 'ctrl', upperCase: 'ctrl' }, { code: 'MetaLeft', lowerCase: 'win', upperCase: 'win' }, { code: 'AltLeft', lowerCase: 'alt', upperCase: 'alt' }, { code: 'Space', lowerCase: 'space', upperCase: 'space' }, { code: 'AltRight', lowerCase: 'alt', upperCase: 'alt' }, { code: 'ControlRight', lowerCase: 'ctrl', upperCase: 'ctrl' }, { code: 'ArrowLeft', lowerCase: '&#9668;', upperCase: '&#9668;' }, { code: 'ArrowDown', lowerCase: '&#9660;', upperCase: '&#9660;' }, { code: 'ArrowRight', lowerCase: '&#9658;', upperCase: '&#9658;' }];
 const buttonTextEnglish = [{ lowerCase: '`', upperCase: '~' }, { lowerCase: '1', upperCase: '!' }, { lowerCase: '2', upperCase: '@' }, { lowerCase: '3', upperCase: '#' }, { lowerCase: '4', upperCase: '$' }, { lowerCase: '5', upperCase: '%' }, { lowerCase: '6', upperCase: '^' }, { lowerCase: '7', upperCase: '&' }, { lowerCase: '8', upperCase: '*' }, { lowerCase: '9', upperCase: '(' }, { lowerCase: '0', upperCase: ')' }, { lowerCase: '-', upperCase: '_' }, { lowerCase: '=', upperCase: '+' }, { lowerCase: 'backspace', upperCase: 'backspace' },
   { lowerCase: 'tab', upperCase: 'tab' }, { lowerCase: 'q', upperCase: 'Q' }, { lowerCase: 'w', upperCase: 'W' }, { lowerCase: 'e', upperCase: 'E' }, { lowerCase: 'r', upperCase: 'R' }, { lowerCase: 't', upperCase: 'T' }, { lowerCase: 'y', upperCase: 'Y' }, { lowerCase: 'u', upperCase: 'U' }, { lowerCase: 'i', upperCase: 'I' }, { lowerCase: 'o', upperCase: 'O' }, { lowerCase: 'p', upperCase: 'P' }, { lowerCase: '[', upperCase: '{' }, { lowerCase: ']', upperCase: '}' }, { lowerCase: '\\', upperCase: '|' }, { lowerCase: 'del', upperCase: 'del' },
-  { lowerCase: 'caps lock', upperCase: 'caps lock' }, { lowerCase: 'a', upperCase: 'A' }, { lowerCase: 's', upperCase: 'S' }, { lowerCase: 'd', upperCase: 'D' }, { lowerCase: 'f', upperCase: 'F' }, { lowerCase: 'g', upperCase: 'G' }, { lowerCase: 'h', upperCase: 'H' }, { lowerCase: 'j', upperCase: 'J' }, { lowerCase: 'k', upperCase: 'K' }, { lowerCase: 'l', upperCase: 'L' }, { lowerCase: ';', upperCase: ':' }, { lowerCase: '\'', upperCase: '\"' }, { lowerCase: 'enter', upperCase: 'enter' },
+  { lowerCase: 'caps lock', upperCase: 'caps lock' }, { lowerCase: 'a', upperCase: 'A' }, { lowerCase: 's', upperCase: 'S' }, { lowerCase: 'd', upperCase: 'D' }, { lowerCase: 'f', upperCase: 'F' }, { lowerCase: 'g', upperCase: 'G' }, { lowerCase: 'h', upperCase: 'H' }, { lowerCase: 'j', upperCase: 'J' }, { lowerCase: 'k', upperCase: 'K' }, { lowerCase: 'l', upperCase: 'L' }, { lowerCase: ';', upperCase: ':' }, { lowerCase: '\'', upperCase: '"' }, { lowerCase: 'enter', upperCase: 'enter' },
   { lowerCase: 'shift', upperCase: 'shift' }, { lowerCase: 'z', upperCase: 'Z' }, { lowerCase: 'x', upperCase: 'X' }, { lowerCase: 'c', upperCase: 'C' }, { lowerCase: 'v', upperCase: 'V' }, { lowerCase: 'b', upperCase: 'B' }, { lowerCase: 'n', upperCase: 'N' }, { lowerCase: 'm', upperCase: 'M' }, { lowerCase: ',', upperCase: '<' }, { lowerCase: '.', upperCase: '>' }, { lowerCase: '/', upperCase: '?' }, { lowerCase: '&#9650;', upperCase: '&#9650;' }, { lowerCase: 'shift', upperCase: 'shift' },
   { lowerCase: 'ctrl', upperCase: 'ctrl' }, { lowerCase: 'win', upperCase: 'win' }, { lowerCase: 'alt', upperCase: 'alt' }, { lowerCase: 'space', upperCase: 'space' }, { lowerCase: 'alt', upperCase: 'alt' }, { lowerCase: 'ctrl', upperCase: 'ctrl' }, { lowerCase: '&#9668;', upperCase: '&#9668;' }, { lowerCase: '&#9660;', upperCase: '&#9660;' }, { lowerCase: '&#9658;', upperCase: '&#9658;' }];
 
@@ -40,6 +40,91 @@ let englishValue = localStorage.getItem('english');
 let shiftPressed = false;
 let isKeyDown = false;
 let keyboardRow;
+/* Add items to the rows */
+function addItemsToTheRow(from, to) {
+  for (let j = from; j < to; j += 1) {
+    const keyboardButton = document.createElement('div');
+    keyboardButton.classList.add('keyboard-button');
+    if (englishValue === 'true' && shiftPressed) {
+      keyboardButton.innerHTML = buttonTextEnglish[j].upperCase;
+    } else if (englishValue === 'true' && !shiftPressed) {
+      keyboardButton.innerHTML = buttonTextEnglish[j].lowerCase;
+    } else if (englishValue === 'false' && !shiftPressed) {
+      keyboardButton.innerHTML = buttonTextRussian[j].lowerCase;
+    } else if (englishValue === 'false' && shiftPressed) {
+      keyboardButton.innerHTML = buttonTextRussian[j].upperCase;
+    }
+    const inner = keyboardButton.innerHTML;
+    /* Give special buttons special classes */
+    if (inner.length !== 1) {
+      keyboardButton.innerHTML = keyboardButton.innerHTML.toUpperCase();
+      keyboardButton.classList.add(`keyboard-button--${inner.slice(0, 3)}`);
+    } else {
+      keyboardButton.classList.add('keyboard-button--letter');
+    }
+    keyboardRow.append(keyboardButton);
+  }
+}
+
+
+/* Add animation on hover */
+function buttonHover(el) {
+  el.classList.add('keyboard-button--hover');
+}
+/* Remove animation on hover */
+function buttonHoverRemove(el) {
+  el.classList.remove('keyboard-button--hover');
+}
+/* Add animation on pressed */
+function buttonPressed(el) {
+  el.classList.add('keyboard-button--active');
+  isKeyDown = true;
+}
+/* Remove animation on pressed */
+function buttonPressedRemove() {
+  if (isKeyDown) {
+    const button = document.querySelectorAll('.keyboard-button');
+    button.forEach((item) => {
+      item.classList.remove('keyboard-button--active');
+    });
+    isKeyDown = false;
+  }
+}
+
+/* Implement Caps Lock */
+let areValuesUppercase = false;
+function uppercaseValuesStable() {
+  const keyboardButton = document.querySelectorAll('.keyboard-button--letter');
+  if (areValuesUppercase) {
+    areValuesUppercase = false;
+    keyboardButton.forEach((item) => {
+      const el = item;
+      el.innerHTML = el.innerHTML.toLowerCase();
+    });
+  } else {
+    areValuesUppercase = true;
+    keyboardButton.forEach((item) => {
+      const el = item;
+      el.innerHTML = el.innerHTML.toUpperCase();
+    });
+  }
+}
+
+/* Add value of keyboard button to textarea */
+function addLetterToTextarea(event) {
+  let text = document.querySelector('.textarea').value;
+  text += event.currentTarget.innerText;
+  document.querySelector('.textarea').value = text;
+}
+
+/* Implement one function for Backspace, Delete, Space, Tab, Enter */
+function selectionFunction(value, selectStart, selectEnd) {
+  if ((selectStart !== -1) || (selectStart === -1 && textarea.value !== '')) {
+    textarea.setRangeText(value, textarea.selectionStart + selectStart, textarea.selectionEnd + selectEnd, 'end');
+    textarea.focus();
+  }
+}
+
 function buildKeyboard() {
   /* Delete keyboard before building it */
   const allKeyboardRows = document.querySelectorAll('.keyboard-row');
@@ -47,7 +132,7 @@ function buildKeyboard() {
     item.remove();
   });
   /* Build rows */
-  for (let i = 1; i < 6; i++) {
+  for (let i = 1; i < 6; i += 1) {
     keyboardRow = document.createElement('div');
     keyboardRow.classList.add('keyboard-row');
     keyboardRow.classList.add(`keyboard-row-${[i]}`);
@@ -67,7 +152,15 @@ function buildKeyboard() {
   /* Add Event Listeners to shift */
   const shift = document.querySelectorAll('.keyboard-button--shi');
   shift.forEach((item) => {
-    item.addEventListener('click', uppercaseValuesFlexible);
+    item.addEventListener('click', () => {
+      if (shiftPressed) {
+        shiftPressed = false;
+        buildKeyboard();
+      } else {
+        shiftPressed = true;
+        buildKeyboard();
+      }
+    });
   });
   /* Add Event Listeners to virtual buttons */
   const button = document.querySelectorAll('.keyboard-button');
@@ -91,10 +184,9 @@ function buildKeyboard() {
 
   /* Add Event Listener keydown to buttons to make them active */
   document.addEventListener('keydown', (event) => {
-    const button = document.querySelectorAll('.keyboard-button');
     const key = event.code;
     button.forEach((item, i) => {
-      if (buttonTextRussian[i].code == key) {
+      if (buttonTextRussian[i].code === key) {
         item.classList.add('keyboard-button--active');
       }
     });
@@ -102,45 +194,19 @@ function buildKeyboard() {
 }
 buildKeyboard();
 
-/* Add items to the rows */
-function addItemsToTheRow(from, to, language, shift) {
-  for (let j = from; j < to; j++) {
-    const keyboardButton = document.createElement('div');
-    keyboardButton.classList.add('keyboard-button');
-    if (englishValue == 'true' && shiftPressed) {
-      keyboardButton.innerHTML = buttonTextEnglish[j].upperCase;
-    } else if (englishValue == 'true' && !shiftPressed) {
-      keyboardButton.innerHTML = buttonTextEnglish[j].lowerCase;
-    } else if (englishValue == 'false' && !shiftPressed) {
-      keyboardButton.innerHTML = buttonTextRussian[j].lowerCase;
-    } else if (englishValue == 'false' && shiftPressed) {
-      keyboardButton.innerHTML = buttonTextRussian[j].upperCase;
-    }
-    const inner = keyboardButton.innerHTML;
-    /* Give special buttons special classes */
-    if (inner.length !== 1) {
-      keyboardButton.innerHTML = keyboardButton.innerHTML.toUpperCase();
-      keyboardButton.classList.add(`keyboard-button--${inner.slice(0, 3)}`);
-    } else {
-      keyboardButton.classList.add('keyboard-button--letter');
-    }
-    keyboardRow.append(keyboardButton);
-  }
-}
-
 document.addEventListener('keydown', (event) => {
   /* Make pressed buttons active */
   const button = document.querySelectorAll('.keyboard-button');
   const key = event.code;
   button.forEach((item, i) => {
-    if (buttonTextRussian[i].code == key) {
+    if (buttonTextRussian[i].code === key) {
       item.classList.add('keyboard-button--active');
     }
   });
   textarea.focus();
   /* Switch languages */
   if (event.ctrlKey && event.altKey) {
-    if (englishValue == 'true') {
+    if (englishValue === 'true') {
       englishValue = localStorage.english = 'false';
       buildKeyboard();
     } else {
@@ -164,78 +230,10 @@ document.addEventListener('keyup', (event) => {
   const button = document.querySelectorAll('.keyboard-button');
   const key = event.code;
   button.forEach((item, i) => {
-    if (buttonTextRussian[i].code == key) {
+    if (buttonTextRussian[i].code === key) {
       item.classList.remove('keyboard-button--active');
     }
   });
 });
 /* Remove active class from buttons when they are not clicked */
 window.addEventListener('mouseup', buttonPressedRemove);
-/* Implement shift */
-function uppercaseValuesFlexible() {
-  if (shiftPressed) {
-    shiftPressed = false;
-    buildKeyboard();
-  } else {
-    shiftPressed = true;
-    buildKeyboard();
-  }
-}
-
-/* Implement Caps Lock */
-let areValuesUppercase = false;
-function uppercaseValuesStable() {
-  const keyboardButton = document.querySelectorAll('.keyboard-button--letter');
-  if (areValuesUppercase) {
-    areValuesUppercase = false;
-    keyboardButton.forEach((item) => {
-      item.innerHTML = item.innerHTML.toLowerCase();
-    });
-  } else {
-    areValuesUppercase = true;
-    keyboardButton.forEach((item) => {
-      item.innerHTML = item.innerHTML.toUpperCase();
-    });
-  }
-}
-
-/* Add value of keyboard button to textarea */
-function addLetterToTextarea() {
-  let text = document.querySelector('.textarea').value;
-  text += event.currentTarget.innerText;
-  const button = document.querySelectorAll('.keyboard-button');
-  document.querySelector('.textarea').value = text;
-}
-
-/* Implement one function for Backspace, Delete, Space, Tab, Enter */
-function selectionFunction(value, selectStart, selectEnd) {
-  if ((selectStart != -1) || (selectStart == -1 && textarea.value != '')) {
-    textarea.setRangeText(value, textarea.selectionStart + selectStart, textarea.selectionEnd + selectEnd, 'end');
-    textarea.focus();
-  }
-}
-
-/* Add animation on hover */
-function buttonHover(el) {
-  el.classList.add('keyboard-button--hover');
-}
-/* Remove animation on hover */
-function buttonHoverRemove(el) {
-  el.classList.remove('keyboard-button--hover');
-}
-/* Add animation on pressed */
-function buttonPressed(el) {
-  const button = document.querySelectorAll('.keyboard-button');
-  el.classList.add('keyboard-button--active');
-  isKeyDown = true;
-}
-/* Remove animation on pressed */
-function buttonPressedRemove() {
-  if (isKeyDown) {
-    const button = document.querySelectorAll('.keyboard-button');
-    button.forEach((item) => {
-      item.classList.remove('keyboard-button--active');
-    });
-    isKeyDown = false;
-  }
-}
